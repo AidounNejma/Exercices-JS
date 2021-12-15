68432b40e79a987dbaa5cf3dcb907069ac91a6bb
AidounNejma <guermachenejma@gmail.com>
2021-12-15 11:39:33 +0100

    second commit

--- js/exo14.js
+++ js/exo14.js
@@ -15,6 +15,11 @@ $(document).ready(function() {
         $( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
     }
     });
+    
     $( "#amount" ).val( "€" + $( "#slider-range" ).slider( "values", 0 ) +
     " - €" + $( "#slider-range" ).slider( "values", 1 ) );
+
+    $("#result").val = parseInt($( "#slider-range" ).slider( "values", 1 ) - $( "#slider-range" ).slider( "values", 0 ));
+
 } );
+
